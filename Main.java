448c48bd5ff663a4eb6ccd637d7a6d36a0cb7016
Rushil Jain <71635499+14rushiljain@users.noreply.github.com>
2026-04-29 22:53:35 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import database.DatabaseHelper;
+import view.LoginFrame;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        DatabaseHelper.initialize();
+
+        java.awt.EventQueue.invokeLater(() -> {   //EDT ( Event Dispatch thread )  
+            new LoginFrame().setVisible(true);
+        });
+    }
+}
